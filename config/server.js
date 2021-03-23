dc1cc6c95111b314036df4b46b394394d2d19f56
Subliminal Guy <subliminal_kid@posteo.de>
2021-03-23 16:22:22 +0100

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0833f10477586fc03ae04b1a7a36dadc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '13f30235-45a6-42bd-8743-73eb320993b6'
+};
